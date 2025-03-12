9f669f7a5ab49233fbbbde04aa82fb43e408743b
PrinMax <max.prin@edu.univ-fcomte.fr>
2025-03-12 19:24:11 +0100

    test commit

--- src/js/backend.js
+++ src/js/backend.js
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase('http://127.0.0.1:8090'); // Remplacez par l'URL de votre instance PocketBase
+export const pb = new PocketBase('http://127.0.0.1:8090'); // Remplacez par l'URL de votre instance PocketBase
 
 // Vérifier si un utilisateur est déjà connecté
 if (pb.authStore.isValid) {
